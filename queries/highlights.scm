c77bb9a87148c22e45e69c6a258185214878e88d
Lucas Van Laer <lucas.van.laer@gmail.com>
2023-04-22 20:01:58 +0200

    add highlights

---
+++ queries/highlights.scm
@@ -0,0 +1,23 @@
+(function) @function
+(procedure_decl
+  (name)) @function
+(type_decl
+  (name)) @type
+(value
+  (name)) @type
+
+[
+ "True"
+ "False"
+] @constant.builtin
+
+[
+ (int)
+ (real)
+] @number
+
+(comment) @comment
+
+[
+ "type"
+] @keyword
